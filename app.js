cc25040e41f997030574aa92aed6fb290f76db23
daniel <danny.alarcon1997@gmail.com>
2022-10-28 20:02:23 -0400

    setting up sign up page

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/html/signup.html");
+});
+
+app.post("/", function (req, res) {
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+  console.log(firstName, lastName, email);
+});
+
+app.listen(port, function () {
+  console.log(`Server is running on port ${port}`);
+});
